c66ca796e002bb997aca18a8888c314112a4769b
juanmanuel0 <86835421+juanmanuel0@users.noreply.github.com>
2021-07-06 11:09:35 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,10 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("esto funciona de maravilla");
+
+    
+	alert("esto es otro alert")
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,15 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	
+	let nombre; //declaro variable 
+	
+	nombre = prompt("pone tu nombre")
+
+	alert(nombre)
+
+	 
+
+	
 }
 
