399c44644a6ec23b8f439b071d95b49cdd5ea18d
juanmanuel0 <86835421+juanmanuel0@users.noreply.github.com>
2021-07-07 00:08:12 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,17 +3,9 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombre;
-	
-nombre = document.getElementById("txtIdNombre").value;
-
-alert(nombre)
-
-
-
-
-
-
+	var nombreIngresado;
+	nombreIngresado=txtIdNombre.value;
+	alert("ok");
 
 }
 
