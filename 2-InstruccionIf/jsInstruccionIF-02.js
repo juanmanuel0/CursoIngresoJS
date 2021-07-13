a34f3c7fd15a61ad940438b8ae88600e48b77740
juanmanuel0 <86835421+juanmanuel0@users.noreply.github.com>
2021-07-13 00:43:53 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,15 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if( edad >= 18)
+
+	{ 
+		alert("mayor de edad")
+	}
+ 
 	
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓ
--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,22 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if( edad >= 18)
+
+
+	{ 
+		alert("mayor de edad");
+	}
+	 else 
+	 {
+
+		alert("menor de edad");
+
+
+	 }
+	
 
 }//FIN DE LA FUNCIÓN
