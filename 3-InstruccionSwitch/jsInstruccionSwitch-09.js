aeae94fffe406b4b04ead2eb1db9bd8de31d4cf5
juanmanuel0 <86835421+juanmanuel0@users.noreply.github.com>
2021-07-15 08:37:47 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -2,10 +2,9 @@ function mostrar()
 {
 	let estacion;
 	let destino;
-	let amuento = 0;
-	let descuento = 0;
+	
 	let presiofinal;
-	const PRECIO = 15000;
+	let precio = 15000;
 
 	estacion = document.getElementById("txtIdEstacion").value;
 	destino = document.getElementById("txtIdDestino").value;
@@ -13,63 +12,54 @@ function mostrar()
 	switch (estacion) {
 		 case "Invierno":
 			 if(destino == "Bariloche"){
-				amuento = 20;
-
-
-			 }else if (destino == "Mar del plata") {
-				 descuento = 20;
+				presiofinal = precio + precio * 0.20;
+             }else if (destino == "Mar del plata") {
+				 presiofinal = precio - precio * .20;
 			 }else if (destino == "Cordoba") {
-				 descuento = 10;
+				presiofinal = precio - precio * .10;
 			 }else if (destino == "Cataratas") {
-				 descuento = 10;
+				presiofinal = precio - precio * .10;
 			 }
 
 			  break;
 		 case "Verano":
 			if(destino == "Bariloche"){
-                descuento = 20;
+                presiofinal = precio - precio * .10;
 			}else if (destino == "Mar del plata") {
-				amuento = 20;
+				presiofinal = precio + precio * .20;
 			}else if (destino == "Cordoba") {
-				aumento = 10;
+				presiofinal = precio + precio * .10;
 			}else if (destino == "Cataratas") {
-				aumento = 10;
+				presiofinal = precio + precio * .10;
 			} 
 
 				break;
 		case "Otoño":
 			if(destino == "Bariloche"){
-                    amuento = 10;
+				presiofinal = precio + precio * .10;
 			}else if (destino == "Mar del plata") {
-				amuento = 10;
+				presiofinal = precio + precio * .10;
 			}else if (destino == "Cordoba") {
-				presiofinal = PRECIO
+				presiofinal = precio
 			}else if (destino == "Cataratas") {
-				aumento = 10;
+				presiofinal = precio + precio * .10;
 			}
 		case "Primavera":
 			if(destino == "Bariloche"){
-				amuento = 10;
+				presiofinal = precio + precio * .10;
 		}else if (destino == "Mar del plata") {
-			amuento = 10;
+			presiofinal = precio + precio * .10;
 		}else if (destino == "Cordoba") {
 			presiofinal = PRECIO
 		}else if (destino == "Cataratas") {
-			aumento = 10;
+			presiofinal = precio + precio * .10;
 		}
 			  break;	 
 			 
 	 
 		
 	 }
-	 if (amuento != 0) {
-		 presiofinal = PRECIO + PRECIO * amuento /100;
-	 }else if (descuento != 0) {
-		 presiofinal = PRECIO - PRECIO * descuento /100;
-	 
-	 }
-	 alert("presio fianl $ " + presiofinal);
-	
+	 alert("el precio final es $ " + presiofinal);
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -2,6 +2,10 @@ function mostrar()
 {
 	let destinos;
 	let estacion;
+	let si;
+	let no;
+
+	
 	 
     destinos = document.getElementById("txtIdDestino").value;
 	estacion = document.getElementById("txtIdEstacino").value;
@@ -9,13 +13,13 @@ function mostrar()
 	switch (estacion) {
 		case "Invierno":
 			if (destino == "Bariloche") {
-				
-			}
+				alert("puto")
+			}else{no;}
 			break;
 	    case "Verano":
-			if (destino == "Mar del plata") {
-				
-			}
+			if (destino == "Mar del plata" || "Cataras") {
+				si;
+			}else{ no; }
 			break;
 		case "Otoño":
 			if (destino == "Bariloche") {
@@ -24,9 +28,18 @@ function mostrar()
 			break;
 		case "Primavera":
 			if (destino == "Bariloche") {
-				
-			}
+				no;
+			}else{si;}
 			break;	
 	}
+	if (si) {
+		alert("se viaja");
+	} else if (no) {
+		alert("no se viaja");
+	}{
+		
+	}
+	
+	
 
 }//FIN DE LA FUNCIÓN
