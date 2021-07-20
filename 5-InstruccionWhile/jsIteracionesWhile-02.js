b69276505798dbfa6792b4abd4e3ffdc5dd12e5e
juanmanuel0 <86835421+juanmanuel0@users.noreply.github.com>
2021-07-20 12:49:23 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,8 +3,11 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var contador;
-	contador=0; 
-	alert('iteración while');
-
+	let control = 1;
+	let mensaje = " ";
+	while (control < 11) {
+		mensaje = " " + control + mensaje;
+		control ++;
+	}
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,7 +2,11 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
-	
+	let clave;
+	clave= prompt("ingrese el número clave.");
+	while (clave !=  "utn750") {
+		clave= prompt("la clave es incorrecta intente de nuevo");
+		
+	}
+	alert("Clave correcta");
 }//FIN DE LA FUNCIÓN
