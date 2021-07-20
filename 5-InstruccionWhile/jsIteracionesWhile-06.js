e0597de764c88c17259f48b4c7a9134e2f8cfb3c
juanmanuel0 <86835421+juanmanuel0@users.noreply.github.com>
2021-07-20 10:23:41 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,26 @@
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	let  num;
+	let contador = 0;
+	let acumulador = 0;
+	let suma;
+	let prom;
 
-	contador=0;
-	acumulador=0;
+	while ( contador <= 5) {
+		num = parseInt(prompt("ingresar numero"));
+		contador = contador + 1;
+		acumulador = acumulador + num;
+		contador++;
+
+	}
+	suma = num1 + num2 + num3 + num4 + num5;
+
+	prom = suma/5;
+
+	document.getElementById("txtIdSuma").value = suma;
+	document.getElementById("txtIdPromedio").value = prom;
+
 	
 	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+	
+}//FIN DE LA FUNCIÓN 
