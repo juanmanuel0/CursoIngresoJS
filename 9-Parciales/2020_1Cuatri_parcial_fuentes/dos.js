6e2bebe59c40bdcc538d4b7d705d8eadecca1d58
juanmanuel0 <86835421+juanmanuel0@users.noreply.github.com>
2021-07-29 11:58:42 -0300

    Add files via upload

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,138 @@
 function mostrar()
 {
-  alert("dos");
+  let tipo;
+  let cantidad;
+  let precio;
+  let seguir;
+  let descuento = 0;
+  let importeBruto = 0;
+  let importe;
+  let importeNeto;
+  let acumBolsas = 0;
+  let mayorPrecio;
+  let mayortipo;
+  let flag = 1;
+  let acumuladorBA = 0;
+  let acumuladorBC = 0;
+  let acumuladorBCal = 0;
+  let tipoMaxBolsas;
+  let cantBolsas;
+
+  do {
+    tipo = prompt("ingrese un material (arena/cemento/cal)").toLowerCase();
+    while (tipo != "arena" && tipo != "cemento" && tipo != "cal") {
+      tipo = prompt("Error ingrese un material (arena/cemento/cal)").toLowerCase();
+    }
+    cantidad = parseInt(prompt("ingrese cantidad"))
+    while (isNaN(cantidad && cantidad > 0)) {
+      cantidad = parseInt(prompt("canitdad invalida. ingrese cantidad"))  
+    }
+    precio = parseInt(prompt("ingrese pecio"))
+    while (isNaN(precio && precio > 0)) {
+      precio = parseInt(prompt("canitdad invalida. ingrese precio"))  
+    }
+
+    acumBolsas = acumBolsas + cantidad;
+
+    importe = precio * cantidad;
+
+    importeBruto += importe
+    if (tipo == "arena") {
+      acumuladorBA++;
+    } else if (tipo == "cal") {
+      acumuladorBCal++;
+    }else if (tipo == "cemento"){
+      acumuladorBC++;
+    }
+    if (flag || precio > mayorPrecio) {
+      mayorPrecio = precio;
+      mayortipo = tipo;
+      flag = 0;}
+    
+  
+  
+    seguir = prompt("quiere seguir (si/no)");
+  } while (seguir == "si");
+
+  if (acumBolsas > 30){
+    descuento = importeBruto * .25;
+  }else if (acumBolsas > 10){
+    descuento = importeBruto * .15;
+  }
+  
+  importeNeto = importeBruto - descuento;
+
+  if (acumladorBC > acumuladorBA && acumuladorBC > acumuladorBCal) {
+    tipoMaxBolsas = "Cenmento";
+    cantBolsas = acumladorBC
+  }else if (acumuladorBA >= acumuladorBC && acumuladorBA > acumuladorBCal){
+    tipoMaxBolsas = "Arena"
+    cantBolsas = acumuladorBA
+
+  }else{
+    tipoMaxBolsas = "Cal";
+    cantBolsas = acumuladorBCal}
+
+    document.write("a) el importe total a pagar es " + importeBruto + "<br>");
+    if(descuento > 0){
+      document.write("b) el importe total a pagar con descuento " + importeNeto + "<br>");
+    }
+    document.write("c) tipo con mas cantidad de bolsas " + tipoMaxBolsas + " con " +
+    cantBolsas + " bolsas<br>")
+    document.write("d) el tipo mas caro " + mayortipo + " precio " + mayorPrecio + "<br>" );
+
+
+ 
 }
+/*
+Estrategia de soluciom
+1- declarar de variables
+cosas que le pediomos al usuario -> tipo / cantidad / precio / seguir
+cosas que no tengo que pedir al usuario -> descuento / importeBruto / importeNeto / acumBolsas /
+mayorPrecio / mayoTipo / acumuladorBA / acumuladorBC / acumuladorBCal / maxBolsas / tipoMasBolsas
+cantBolsas / 
+
+2- genero un blucke mientras el usuario quiera(do-while)
+
+3- cosas que se repiten por cada producto comprado
+
+3.1 pido y valido el tipo
+3.2 pido y valido la cantidad (+0)
+3.3 pido y valido precio (+0)
+
+3.4 acumular la cantidad de bolsas 
+3.5 acumular el importe bruto
+
+3.6 me fijo el tipo  de producto y acumulo la cantidad de bolsa segun el tipo
+
+3.7 para el primer procuto gaurdo tipo y precio (es mi unico procucto por ende es el mas caro)
+para los sifuientes productos voy a comparar sus precio con el producto caro y si es mas caro
+actualizo el precio y producto 
+-------------------------------------------------------------------------------------------
+
+despues del bulke 
+me fijo que descuento corresponde (de acuerdo al acumulador de bolsas)
+calculo el descuento
+
+calculo es importe neto 
+
+me fijo cual es mayo acumulador de bolsas para informar el tipo de producto que se compraron
+mas bolsas 
+
+mostras los resultados s
+-------------------------------------------------------------------------------------------
+Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo con mas cantidad de bolsas.
+f) El tipo mas caro
+---------------------------------------------------------
+
+
+*/
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,151 @@
 
 function mostrar()
 {
-	alert("uno");
+	let tipo;
+	let cantidad;
+	let precio;
+	let marca;
+	let fabricante;
+	let preciominA;
+	let contABarato;
+	let fabABarato;
+	let flagA = 1;
+	let contadorB = 0;
+	let contadorA = 0;
+	let contadorJ = 0;
+	let acumuladorB = 0;
+	let acumuladorA = 0;
+	let acumuladorJ = 0;
+	let promedio;
+	let maxUnidades;
+
+	for (let i = 0; i < 5; i++) {
+	    tipo = prompt("Ingrese tipo (barbijo/alcohol/jabon) ").toLowerCase(); 
+	 	while (tipo != "jabon" && tipo != "barbijo" && tipo != "alcohol") {
+			tipo = prompt("Tipo invalido. Reingrese tipo (barbijo/alcohol/jabon) ").toLoleLowerCase(); 
+		}
+		precio = parseInt(prompt("ingrese un precio (100-300)"));
+		while (!(precio >=100 && precio <= 300)) {
+			precio = parseInt(prompt("precio invalido. reingrese un precio (100-300)"));
+		}
+		cantidad = parseInt(prompt("ingrese una cantidad (max 1000)"));
+		while (!(precio > 0 && precio <= 1000)) {
+			precio = parseInt(prompt("cantidad invalida. reingrese una cantidad  (max 1000)"));
+		}
+	    marca = prompt("ingrese una marca");
+	    while (marca == " ") {
+		    marca = prompt("Error. ingrese una marca")
+		}
+	    fabricante = prompt("ingrese un fabricante");
+	    while (fabricante == " ") {
+		    fabricante = prompt("Error. ingrese un fabricante")
+			}	
+		if (tipo == "barbijo") {
+			acumuladorB = acumuladorB + cantidad;
+			contadorB++;
+			
+			alert("Barbijo")
+		} else if (tipo == "alcohol") {
+			acumuladorA = acumuladorA + cantidad;
+			contadorA++
+			if (flagA || precio < preciominA) {
+				preciominA = precio;
+				fabABarato = fabricante;
+				contABarato = cantidad;
+				flagA = 0;
+			}
+			
+		} else if(tipo == "jabon"){
+			acumuladorJ = acumuladorJ + cantidad;
+			contadorJ++
+			} 
+			
+			
+	}//fin del for
+	if (acumuladorJ > acumuladorA && acumuladorJ > acumuladorB) {
+		maxUnidades = "jabon";
+		premedio = acumuladorJ / contadorJ;
+	}else if (acumuladorA >= acumuladorJ && acumuladorA > acumuladorB) {
+		maxUnidades = "Alcohol";
+		promedio = acumuladorA / contadorA;
+	}else{
+		maxUnidades = "Barbijo";
+		promedio = acumuladorB / contadorB;
+	}
+	if (flagA) {
+		document.write("A) No se ingresaron alcoholes")
+	} else {
+		
+	}
+
+
+	document.write("a) precio alcohol barato " + preciominA + " fab " + fabABarato + " cant " + contABarato + "<br>")
+	document.write("b) tipo con mas unidad  " + maxUnidades + " promedio " + promedio + "<br>")
+	document.write("c) hay " + acumuladorJ + " de jabon" + "<br>")
+
+
+	
 }
+/*
+1 - declarar variables (tipo/ cantidad / precio / marca / fabricante ) esto se lo pediomos al usuario
+a) let preciominA / contABarato / fabABarato / flagA
+b) contadorB / contadorA / contadorJ / maxUnidades / acumB / acumaA / acumJ / promedio 
+c) acumJ (ya esta declarodo en el b)
+-----------------------------------------------------------
+2- generar un bluke que se repita 5 veces (for) porque se las veces que se tiene que repetir
+2.1- cosas que se tiene que hacer 5 veces (van dentro del for)
+2.1.1- pido tipo y valido
+2.1.2- pido precio y valido
+2.1.3- pido cantidad y valido
+2.1.4- pido marca
+2.1.5- pido fabricante
+
+a) me fijo si es producto es de tipo alcohol y si es asi la primera vez que ingresa alcohol guardo
+el precio en precio barato y las siguiete veces que ingrese un alcohol lo comparo con el precio
+del alcohol barato y si es mas barato lo actualizo(tambien guardo cantidad y fabricamte)
+
+b)me fijo de que tipo es el producto y actualizo acumulador y contador del producto
+
+c)si es de tipo jabon lo acumulo(ya lo hize en el punto b)
+--------------------------------------------------------
+3.1-cosas que hago una sola vez despues del for
+3.1.1 -para el punto b me tengoq que fijar cual es el mayor acumulador y sacar el promedio para ese tipo
+3.2-muestro los resultados 
+----------------------------------
+Debemos realizar la carga de 5(cinco) productos de prevención de contagio,
+de cada una debo obtener los siguientes datos:
+el tipo (validar "barbijo" , "jabón" o "alcohol") ,
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+la Marca y el fabricante.
+Se debe Informar al usuario lo siguiente:
+a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+b) Del tipo con mas unidades, el promedio por compra
+c) Cuántas unidades de jabones hay en total
+
+---------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
